89ff424741d199163f41804ebe1e3d5b14009166
Carlos_Eduardo_Alvear_Mutis_ <alvearmutis@outlook.es>
2022-09-09 17:23:46 -0500

    parcial 1

---
+++ Exams/Exam-01/EmployeeApp/Employee.DAL/Context/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Employee.DAL.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Entities.Models.Employee> Employees { get; set; }
+    }
+}
