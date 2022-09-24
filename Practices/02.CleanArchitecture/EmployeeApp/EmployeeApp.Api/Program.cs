b54356647b1799fcfebf850b15ee3c4f697e6117
Carlos_Eduardo_Alvear_Mutis_ <alvearmutis@outlook.es>
2022-09-24 16:45:54 -0500

    a

--- Practices/02.CleanArchitecture/EmployeeApp/EmployeeApp.Api/Program.cs
+++ Practices/02.CleanArchitecture/EmployeeApp/EmployeeApp.Api/Program.cs
@@ -1,3 +1,8 @@
+using EmployeeApp.Api.Extensions;
+using EmployeeApp.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,19 +11,48 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CnxStr")));
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Employee.Api", Version = "v1" });
+});
+
+// Add Modules
+builder.Services.AddCoreModules();
+builder.Services.AddInfrastructureModules();
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-   app.UseSwagger();
-   app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee.Api v1"));
 }
+
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
+
+app.UseCors(options => {
+    options.WithOrigins("http://localhost:3000");
+    options.AllowAnyMethod();
+    options.AllowAnyHeader();
+});
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseHttpsRedirection();
 
 app.MapControllers();
 
--- Practices/02.CleanArchitecture/EmployeeApp/EmployeeApp.Domain/Entities/Employee.cs
+++ Practices/02.CleanArchitecture/EmployeeApp/EmployeeApp.Domain/Entities/Employee.cs
@@ -1,19 +1,25 @@
 ﻿using EmployeeApp.Domain.Common;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace EmployeeApp.Domain.Entities
 {
-   public class Employee : EntityBase
-   {
-      [Required]
-      public string? FirstName { get; set; }
+    public class Employee : EntityBase
+    {
 
-      [Required]
-      public string? LastName { get; set; }
+        [Key]
+        public int Id { get; set; }
 
-      public DateTime HireDate { get; set; }
+        [Required]
+        public string FirstName { get; set; }
 
-      [Required]
-      public string? Department { get; set; }
-   }
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        [Required]
+        [RegularExpression(@"[IT, Finance, Audit]", ErrorMessage = "Invalid Department")]
+        public string? Department { get; set; }
+    }
 }
