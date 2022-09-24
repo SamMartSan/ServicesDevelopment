3aa3b5eb72948a77e844f0172595488d2543e89e
Carlos_Eduardo_Alvear_Mutis_ <alvearmutis@outlook.es>
2022-09-24 17:46:35 -0500

    practica Terminada

---
+++ Practices/02.CleanArchitecture/EmployeeApp/EmployeeApp.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeApp.Application.Interfaces;
+using EmployeeApp.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeApp.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        // GET: api/<PeopleController>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _employeeService.GetAllAsync());
+        }
+
+        // GET api/<PeopleController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _employeeService.GetByIdAsync(id));
+        }
+
+        // POST api/<PeopleController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Employee employee)
+        {
+            return Ok(await _employeeService.AddAsync(employee));
+        }
+
+        // PUT api/<PeopleController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Employee employee)
+        {
+            await _employeeService.UpdateAsync(id, employee);
+            return Ok();
+        }
+
+        // DELETE api/<PeopleController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _employeeService.RemoveAsync(id);
+            return Ok();
+        }
+    }
+}
